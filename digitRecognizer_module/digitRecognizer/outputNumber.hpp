3feef34dc0fdd51fe1b8d94f8ed0511aca3adf8c
Sun Xi <xis@xis.wv.cc.cmu.edu>
2017-04-12 19:39:43 -0400

    digit recognizing module created so the function can be called directly to generate results

---
+++ digitRecognizer_module/digitRecognizer/outputNumber.hpp
@@ -0,0 +1,22 @@
+//
+//  outputNumber.hpp
+//  digitRecognizer
+//
+//  Created by Sun Xi on 12/04/2017.
+//  Copyright © 2017 SUNXI. All rights reserved.
+//
+
+#ifndef outputNumber_hpp
+#define outputNumber_hpp
+
+#include <stdio.h>
+#include <opencv2/opencv.hpp>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/ml/ml.hpp"
+#include <stdlib.h> // Include the standard library
+
+using namespace cv;
+
+int recognize(std::string imgDir);
+
+#endif /* outputNumber_hpp */
