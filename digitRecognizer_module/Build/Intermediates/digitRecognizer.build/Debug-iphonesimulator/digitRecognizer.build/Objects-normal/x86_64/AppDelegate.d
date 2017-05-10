ea959c7820b9df493792d147feef9d3a65007891
Henry Yang <yanghanyugg@gmail.com>
2017-05-09 20:06:06 -0400

    clean GitHub directories

--- digitRecognizer_module/Build/Intermediates/digitRecognizer.build/Debug-iphonesimulator/digitRecognizer.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/xis/Desktop/cvSudoku/digitRecognizer_module/digitRecognizer/AppDelegate.m \
-  /Users/xis/Desktop/cvSudoku/digitRecognizer_module/digitRecognizer/AppDelegate.h
--- digitRecognizer_module/Build/Intermediates/digitRecognizer.build/Debug-iphonesimulator/digitRecognizer.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/xis/Desktop/cvSudoku/digitRecognizer_module/digitRecognizer/main.m \
-  /Users/xis/Desktop/cvSudoku/digitRecognizer_module/digitRecognizer/AppDelegate.h
